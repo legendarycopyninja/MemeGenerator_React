5468ff9cb3050656ac30956d189f4b36e695567c
Dilli_Rao <101472151+legendarycopyninja@users.noreply.github.com>
2023-02-04 22:33:34 +0530

    First Commit

---
+++ memegenerator/src/Components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header(){
+    return(
+        <header className="headerContainer">
+            <img src="trollFace.png" alt="sarcastic laughing face"  />
+            <h2>Meme Generator</h2>
+            <img src="reactIcon.png" alt="react Symbol" className="nav-logo"/>
+            <h3>React Project 3</h3>
+        </header>
+    )
+}
