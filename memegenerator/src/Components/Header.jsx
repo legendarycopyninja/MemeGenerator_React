b4e02975c9aaf1f105aff3db616de13f7cbe22b8
Dilli_Rao <101472151+legendarycopyninja@users.noreply.github.com>
2023-02-06 23:21:03 +0530

    commit 1

--- memegenerator/src/Components/Header.jsx
+++ memegenerator/src/Components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header(){
             <img src="trollFace.png" alt="sarcastic laughing face"  />
             <h2>Meme Generator</h2>
             <img src="reactIcon.png" alt="react Symbol" className="nav-logo"/>
-            <h3>React Project 3</h3>
+            <h2>React Project 3</h2>
         </header>
     )
 }
--- memegenerator/src/Components/Meme.jsx
+++ memegenerator/src/Components/Meme.jsx
@@ -1,15 +1,15 @@
 export default function Meme(){
     return (
-        <main>
-            <form className="formPage">
-                <input type="text"
-                placeholder="Top Text" 
-                className="forminput"/>
-                <input type="text"
-                placeholder="Bottom Text" 
-                className="forminput"/>
-                <button type="submit" className="formbutton">Get a new meme image 🖼</button>
-            </form>
-        </main>
+     <main>
+         <form className="formPage">
+            <input type="text"
+            placeholder="Top Text" 
+            className="forminput"/>
+            <input type="text"
+             placeholder="Bottom Text" 
+             className="forminput"/>
+             <button type="submit" className="formbutton">Get a new meme image 🖼</button>
+         </form>
+    </main>
     )
 }
